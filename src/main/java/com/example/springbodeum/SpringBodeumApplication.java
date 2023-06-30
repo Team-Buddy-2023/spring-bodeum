7233b10538b5049690bbe57a2800b25dc4305485
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-06-30 20:59:13 +0900

    Initial commit

---
+++ src/main/java/com/example/springbodeum/SpringBodeumApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbodeum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBodeumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBodeumApplication.class, args);
+	}
+
+}
