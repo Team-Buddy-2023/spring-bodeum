759da3db5aaa9e88bed35a1715b1598b10023400
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-07-28 23:48:56 +0900

    feat: KakaoController 파일 삭제 및 매핑 주소 변경

--- src/main/java/buddy/springbodeum/controller/user/UserController.java
+++ src/main/java/buddy/springbodeum/controller/user/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
 public class UserController {
 
 //    private final UserService userService;
@@ -15,25 +14,15 @@ public class UserController {
         this.kakaoService = kakaoService;
     }
 
-    @RequestMapping(value="/", method= RequestMethod.GET)
+    @RequestMapping(value="/kakao/url", method= RequestMethod.GET)
     public String login(Model model) {
         model.addAttribute("kakaoUrl", kakaoService.getKakaoLogin());
         System.out.println(kakaoService.getKakaoLogin());
         return "index";
     }
 
+    @RequestMapping(value = "/kakao/redirect", method = RequestMethod.GET)
+    public void postCreateForm(@RequestBody String code) {
 
-    // 프론트에서 인가코드 받아오는 url
-//    @GetMapping("/oauth/token")
-//    public User getLogin(@RequestParam("code") String code) {
-//
-//        // 넘어온 인가 코드를 통해 access_token 발급
-//        OauthToken oauthToken = userService.getAccessToken(code);
-//
-//        //(1)
-//        // 발급 받은 accessToken 으로 카카오 회원 정보 DB 저장
-//        String User = userService.saveUser(oauthToken.getAccess_token());
-//
-//        return User;
-//    }
+    }
 }
