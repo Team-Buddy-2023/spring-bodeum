231ee5fa67a9aae90ae5f61c2f22366623bfcf87
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-09-01 23:34:03 +0900

    test: 배포 test

--- src/main/java/buddy/springbodeum/service/user/KakaoService.java
+++ src/main/java/buddy/springbodeum/service/user/KakaoService.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @Service
 public class KakaoService {
