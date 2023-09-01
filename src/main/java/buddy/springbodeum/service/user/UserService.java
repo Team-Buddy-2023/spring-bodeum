71928aaa6caa26f8a724598ebfa41992edeb3342
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-09-01 23:40:27 +0900

    test: 배포 test

--- src/main/java/buddy/springbodeum/service/user/UserService.java
+++ src/main/java/buddy/springbodeum/service/user/UserService.java
@@ -1,14 +1,14 @@
-package buddy.springbodeum.service.user;
-
-import buddy.springbodeum.repository.user.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-
-@Service
-public class UserService {
-
-    @Autowired
-    UserRepository userRepository; //(1)
-
-}
+//package buddy.springbodeum.service.user;
+//
+//import buddy.springbodeum.repository.user.UserRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//
+//
+//@Service
+//public class UserService {
+//
+//    @Autowired
+//    UserRepository userRepository; //(1)
+//
+//}
