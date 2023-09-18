a5025ce70d465b450df0aa3591e9ebd6686f6464
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-09-18 23:34:17 +0900

    배포 관련 파일 주석처리

--- src/main/java/buddy/springbodeum/controller/user/UserController.java
+++ src/main/java/buddy/springbodeum/controller/user/UserController.java
@@ -1,7 +1,7 @@
 package buddy.springbodeum.controller.user;
-//
-//import buddy.springbodeum.service.user.KakaoService;
-//import org.springframework.ui.Model;
+
+import buddy.springbodeum.service.user.KakaoService;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
@@ -9,31 +9,31 @@ import java.io.IOException;
 
 @RestController
 public class UserController {
-//
-//    private final KakaoService kakaoService;
-//
-//    public UserController(KakaoService kakaoService) {
-//        this.kakaoService = kakaoService;
-//    }
-//
-//    @RequestMapping(value="/kakao/url", method= RequestMethod.GET)
-//    public String login(Model model) {
-//        model.addAttribute("kakaoUrl", kakaoService.getKakaoLogin());
-//        System.out.println(kakaoService.getKakaoLogin());
-//        return "index";
-//    }
-//
-//    //백에서 테스트용
-//    @RequestMapping(value = "/kakao/redirect", method = RequestMethod.GET)
-//    public void callback(HttpServletRequest request) throws Exception {
-//        System.out.println(request);
-//    }
-//
-//    //실제로 인가코드는 프론트에서 전달
-//    @RequestMapping(value = "/kakao/callback", method = RequestMethod.GET)
-//    public void kakaoCallback(@RequestBody String code) throws IOException {
-//        String accessToken = kakaoService.getKakaoAccessToken(code);
-//
-//
-//    }
+
+    private final KakaoService kakaoService;
+
+    public UserController(KakaoService kakaoService) {
+        this.kakaoService = kakaoService;
+    }
+
+    @RequestMapping(value="/kakao/url", method= RequestMethod.GET)
+    public String login(Model model) {
+        model.addAttribute("kakaoUrl", kakaoService.getKakaoLogin());
+        System.out.println(kakaoService.getKakaoLogin());
+        return "index";
+    }
+
+    //백에서 테스트용
+    @RequestMapping(value = "/kakao/redirect", method = RequestMethod.GET)
+    public void callback(HttpServletRequest request) throws Exception {
+        System.out.println(request);
+    }
+
+    //실제로 인가코드는 프론트에서 전달
+    @RequestMapping(value = "/kakao/callback", method = RequestMethod.GET)
+    public void kakaoCallback(@RequestBody String code) throws IOException {
+        String accessToken = kakaoService.getKakaoAccessToken(code);
+
+
+    }
 }
--- src/main/java/buddy/springbodeum/domain/user/User.java
+++ src/main/java/buddy/springbodeum/domain/user/User.java
@@ -1,31 +1,31 @@
-//package buddy.springbodeum.domain.user;
-//
-//import lombok.Getter;
-//import lombok.Setter;
-//
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//
-//@Entity(name = "user")
-//@Getter
-//@Setter
-//public class User {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    private String email;
-//    private String nickname;
-//
-//    public User() {
-//
-//    }
-//
-//    public User(String email, String nickname) {
-//        this.email = email;
-//        this.nickname = nickname;
-//    }
-//}
+package buddy.springbodeum.domain.user;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity(name = "user")
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String nickname;
+
+    public User() {
+
+    }
+
+    public User(String email, String nickname) {
+        this.email = email;
+        this.nickname = nickname;
+    }
+}
