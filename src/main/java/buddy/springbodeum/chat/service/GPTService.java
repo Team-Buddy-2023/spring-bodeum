e0e0bdbe2c4350608f4cf37c29801fee83e32e19
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-10-26 22:46:27 +0900

    test: chat gpt 주석처리

--- src/main/java/buddy/springbodeum/chat/service/GPTService.java
+++ src/main/java/buddy/springbodeum/chat/service/GPTService.java
@@ -18,32 +18,33 @@ import java.util.List;
 @Service
 public class GPTService {
 
-    @Value("${chatgpt.api-key}")
+//    @Value("${chatgpt.api-key}")
     private String key;
 
     public String send(Long characterId, String message) {
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        URI uri = UriComponentsBuilder
-                .fromUriString("https://api.openai.com/v1/chat/completions")
-                .build()
-                .encode()
-                .toUri();
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Authorization", "Bearer " + key);
-
-        ArrayList<Message> list = new ArrayList<>();
-
-        list.add(new Message("system","[Role] : I want you to act as a Philosophy.\n[Results] : Answer my + \"" + message + "\" in Korean in 200 characters or less with a metaphorical tone. Be sure to review and make suggestions to make sure your tone isn't awkward. ##Do not remind me what I asked you for## 그리고 대답은 친구처럼 반말로 대답해줘"));
-
-        Body body = new Body("gpt-3.5-turbo", list);
-
-        RequestEntity<Body> httpEntity = new RequestEntity<>(body, httpHeaders, HttpMethod.POST, uri);
-
-        return restTemplate.exchange(httpEntity, String.class).toString();
-    }
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        URI uri = UriComponentsBuilder
+//                .fromUriString("https://api.openai.com/v1/chat/completions")
+//                .build()
+//                .encode()
+//                .toUri();
+//
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.add("Authorization", "Bearer " + key);
+//
+//        ArrayList<Message> list = new ArrayList<>();
+//
+//        list.add(new Message("system","[Role] : I want you to act as a Philosophy.\n[Results] : Answer my + \"" + message + "\" in Korean in 200 characters or less with a metaphorical tone. Be sure to review and make suggestions to make sure your tone isn't awkward. ##Do not remind me what I asked you for## 그리고 대답은 친구처럼 반말로 대답해줘"));
+//
+//        Body body = new Body("gpt-3.5-turbo", list);
+//
+//        RequestEntity<Body> httpEntity = new RequestEntity<>(body, httpHeaders, HttpMethod.POST, uri);
+//
+//        return restTemplate.exchange(httpEntity, String.class).toString();
+        return ""
+;    }
 
     @AllArgsConstructor
     @Data
