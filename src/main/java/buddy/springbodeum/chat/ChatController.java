1f8105f2cce32b628ab5cc2573708eff09b0e402
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-12-10 14:01:14 +0900

    test: 배포 테스트

--- src/main/java/buddy/springbodeum/chat/ChatController.java
+++ src/main/java/buddy/springbodeum/chat/ChatController.java
@@ -54,44 +54,44 @@ public class ChatController {
 
     }
 
-    @Value("${chatgpt.api-key}")
-    private String key;
-    @PostMapping("/chat/send")
-    public ResponseEntity send(@RequestBody String message) {
-
-        RestTemplate restTemplate = new RestTemplate();
-
-        URI uri = UriComponentsBuilder
-                .fromUriString("https://api.openai.com/v1/chat/completions")
-                .build()
-                .encode()
-                .toUri();
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add("Authorization", "Bearer " + key);
-
-        ArrayList<Message> list = new ArrayList<>();
-
-        list.add(new Message("system","[Role] : I want you to act as a Philosophy.\n[Results] : Answer my + \"" + message + "\" in Korean in 200 characters or less with a metaphorical tone. Be sure to review and make suggestions to make sure your tone isn't awkward. ##Do not remind me what I asked you for## 그리고 대답은 친구처럼 반말로 대답해줘"));
-
-        Body body = new Body("gpt-3.5-turbo", list);
-
-        RequestEntity<Body> httpEntity = new RequestEntity<>(body, httpHeaders, HttpMethod.POST, uri);
-
-        return restTemplate.exchange(httpEntity, String.class);
-    }
-
-    @AllArgsConstructor
-    @Data
-    static class Body {
-        String model;
-        List<Message> messages;
-    }
-
-    @AllArgsConstructor
-    @Data
-    static class Message {
-        String role;
-        String content;
-    }
+//    @Value("${chatgpt.api-key}")
+//    private String key;
+//    @PostMapping("/chat/send")
+//    public ResponseEntity send(@RequestBody String message) {
+//
+//        RestTemplate restTemplate = new RestTemplate();
+//
+//        URI uri = UriComponentsBuilder
+//                .fromUriString("https://api.openai.com/v1/chat/completions")
+//                .build()
+//                .encode()
+//                .toUri();
+//
+//        HttpHeaders httpHeaders = new HttpHeaders();
+//        httpHeaders.add("Authorization", "Bearer " + key);
+//
+//        ArrayList<Message> list = new ArrayList<>();
+//
+//        list.add(new Message("system","[Role] : I want you to act as a Philosophy.\n[Results] : Answer my + \"" + message + "\" in Korean in 200 characters or less with a metaphorical tone. Be sure to review and make suggestions to make sure your tone isn't awkward. ##Do not remind me what I asked you for## 그리고 대답은 친구처럼 반말로 대답해줘"));
+//
+//        Body body = new Body("gpt-3.5-turbo", list);
+//
+//        RequestEntity<Body> httpEntity = new RequestEntity<>(body, httpHeaders, HttpMethod.POST, uri);
+//
+//        return restTemplate.exchange(httpEntity, String.class);
+//    }
+//
+//    @AllArgsConstructor
+//    @Data
+//    static class Body {
+//        String model;
+//        List<Message> messages;
+//    }
+//
+//    @AllArgsConstructor
+//    @Data
+//    static class Message {
+//        String role;
+//        String content;
+//    }
 }
