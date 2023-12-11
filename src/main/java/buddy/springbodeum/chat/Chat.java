43f441d2450f7f3e7a42cdcdc4e1b035ec1b68b2
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-12-11 21:18:22 +0900

    test: 배포

---
+++ src/main/java/buddy/springbodeum/chat/Chat.java
@@ -0,0 +1,31 @@
+package buddy.springbodeum.chat;
+
+import buddy.springbodeum.user.User;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity(name = "chat")
+@Getter
+@Setter
+public class Chat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    @ElementCollection
+    private List<String> question;
+
+    @ElementCollection
+    private List<String> answer;
+
+    private LocalDateTime dateTime;
+
+}
