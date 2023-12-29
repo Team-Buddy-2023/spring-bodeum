a02dd96351b7be63124d8612df23197624281996
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-12-29 15:12:24 +0900

    chore: 스프링 시큐리티 의존성 추가, 기본 로그인 화면 제거

--- src/main/java/buddy/springbodeum/SpringBodeumApplication.java
+++ src/main/java/buddy/springbodeum/SpringBodeumApplication.java
@@ -2,8 +2,9 @@ package buddy.springbodeum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringBodeumApplication {
 
 	public static void main(String[] args) {
