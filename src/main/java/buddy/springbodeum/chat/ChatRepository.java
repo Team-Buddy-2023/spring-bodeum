6d7c62c6d0ac0a0519c672f624d4eccf235b9eb2
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-01-14 00:32:28 +0900

    fix: 회원탈퇴 시 유저가 작성한 공유 채팅 삭제

--- src/main/java/buddy/springbodeum/chat/ChatRepository.java
+++ src/main/java/buddy/springbodeum/chat/ChatRepository.java
@@ -2,9 +2,11 @@ package buddy.springbodeum.chat;
 
 
 import buddy.springbodeum.chat.data.Chat;
+import buddy.springbodeum.user.data.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatRepository extends JpaRepository<Chat, Long> {
-
-
+    List<Chat> findByUser(User byUserId);
 }
