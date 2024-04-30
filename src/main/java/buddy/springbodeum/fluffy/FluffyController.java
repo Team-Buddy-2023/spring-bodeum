6b484d83bae2a7b7dbe6715898d4c8d824604055
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-04-30 21:14:43 +0900

    refactor: fluffycontroller crossorigin 수정

--- src/main/java/buddy/springbodeum/fluffy/FluffyController.java
+++ src/main/java/buddy/springbodeum/fluffy/FluffyController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(originPatterns = "https://bodeum.vercel.app/")
 @RequestMapping("/characters")
 public class FluffyController {
 
